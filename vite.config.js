943206230ae2a84e360988d808f6781d285e44f8
svenalpha <svalphaforsale@yahoo.com>
2023-08-31 13:41:15 +0700

    message

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/vr08/',
+  base: '/src/',
   plugins: [react()],
   build: {
     minify: false,
