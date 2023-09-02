3cefca8ad0c6362e7de7a68f93c35e848b7f7e03
svenalpha <svalphaforsale@yahoo.com>
2023-09-02 13:47:31 +0700

    message

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { resolve } from "path"; 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-const outDir = resolve(__dirname,"dist");
+const outDiry = resolve(__dirname,"dist");
+console.log("outDiry = ",outDiry);
+const outDir = resolve(__dirname,"dist/client");
 console.log("outDir = ",outDir);
-const outDirx = resolve(__dirname,"dist/client");
-console.log("outDirx = ",outDirx);
 
 
 export default defineConfig({
