43dafa5ea66b4f86273c7a7e5e6b4812fbc964b8
svenalpha <svalphaforsale@yahoo.com>
2023-09-02 12:35:43 +0700

    message

--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,11 @@ import react from '@vitejs/plugin-react'
 
 
 export default defineConfig({
-  root: "src",
+ 
   base: '/',
   plugins: [react()],
   build: {
-    outDir: "../dist/client/",
+    outDir: "/dist/client/",
     emptyOutDir: true,
     minify: false,
   },
