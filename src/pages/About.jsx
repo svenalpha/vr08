a5f71c54792a13da93feda752c51aca6c76ddb9d
svenalpha <svalphaforsale@yahoo.com>
2023-09-03 17:41:22 +0700

    message

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,7 +6,7 @@ export default function About() {
   return (
     <>
       <h1>About</h1>
-      <h4 className="strClass">inside change no.  18 express installed globally</h4>  
+      <h4 className="strClass">inside change no.  19 express installed globally</h4>  
       <div>{addAndMultiply(1, 2, 3)}</div>
       <div>{multiplyAndAdd(1, 2, 3)}</div>
     </>
