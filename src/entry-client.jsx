7c8816febd4e120dc69fc74f4594fa2df03d39c3
svenalpha <svalphaforsale@yahoo.com>
2023-09-04 14:36:22 +0700

    message

--- src/entry-client.jsx
+++ src/entry-client.jsx
@@ -4,7 +4,7 @@ import { App } from './App'
 
 ReactDOM.hydrateRoot(
   document.getElementById('app'),
-  <BrowserRouter>
+  <BrowserRouter basename="/vr08.onrender.com">
     <App />
   </BrowserRouter>,
 )
