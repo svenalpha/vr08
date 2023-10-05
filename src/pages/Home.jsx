612eb6c0981f3a35c26ebcd2bf6f5d08e546ceb8
svenalpha <svalphaforsale@yahoo.com>
2023-10-05 14:33:52 +0700

    message

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
-import { addAndMultiply } from '../add'
-import { multiplyAndAdd } from '../multiply'
+
+//import { addAndMultiply } from '../add'
+//import { multiplyAndAdd } from '../multiply'
+
 
 export default function Home() {
   return (
     <>
-      <h1>Home no1 add,multiply removed  14:08</h1>
+      <h1>Home no1 add,multiply removed, import removed  14:31</h1>
    {/*   <div>{addAndMultiply(1, 2, 3)}</div>   */}
   {/*    <div>{multiplyAndAdd(1, 2, 3)}</div>  */}
     </>
