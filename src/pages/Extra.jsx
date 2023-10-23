2e6f94e6095243784ab0ecd4995792054bf5b494
svenalpha <svalphaforsale@yahoo.com>
2023-10-23 18:22:31 +0700

    message

--- src/pages/Extra.jsx
+++ src/pages/Extra.jsx
@@ -1,15 +1,15 @@
 
-//import { addAndMultiply } from '../add'
-//import { multiplyAndAdd } from '../multiply'
-//import UseEffectFetchData from "../UseEffectFetchData";
+import { addAndMultiply } from '../add'
+import { multiplyAndAdd } from '../multiply'
+import UseEffectFetchData from "../UseEffectFetchData";
 
 export default function Extra() {
   return (
     <>
       <h1>Extra</h1>
-    {/*  <UseEffectFetchData />
+      <UseEffectFetchData />
       <div>{addAndMultiply(1, 2, 3)}</div>
-  <div>{multiplyAndAdd(1, 2, 3)}</div>  */}
+  <div>{multiplyAndAdd(1, 2, 3)}</div>  
     </>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,14 @@
 
-//import { addAndMultiply } from '../add'
-//import { multiplyAndAdd } from '../multiply'
+import { addAndMultiply } from '../add'
+import { multiplyAndAdd } from '../multiply'
 
 
 export default function Home() {
   return (
     <>
       <h1>Home no6 add,multiply removed, import removed no2  17:43</h1>
-   {/*   <div>{addAndMultiply(1, 2, 3)}</div>   */}
-  {/*    <div>{multiplyAndAdd(1, 2, 3)}</div>  */}
+      <div>{addAndMultiply(1, 2, 3)}</div>   
+      <div>{multiplyAndAdd(1, 2, 3)}</div>  
     </>
   )
 }
